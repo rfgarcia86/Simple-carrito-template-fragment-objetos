00cd0efd16ff5425b6ceb47be9a8e40ce79f886e
unknown <ricardo.86.fernandez@gmail.com>
2022-11-13 21:19:22 -0500

    Simple Carrito primer commit

---
+++ Readme.md
@@ -0,0 +1,18 @@
+# Ejemplo de un carrito de compras simple, haciendouso de objetos y templates.
+
+## Todo con fines educativos
+
+Si puedes mejorar el codigo bienbenido
+
+## template
+
+```html
+<template id="carritoTemplate">
+  <li
+    class="list-group-item d-flex justify-content-between align-items-centers"
+  >
+    <span class="lead"></span>
+    <span class="badge bg-primary rounded-pill"></span>
+  </li>
+</template>
+```
---
+++ carrito.js
@@ -0,0 +1,46 @@
+const carrito = document.getElementById("carrito")
+const carritoTemplate = document.getElementById("carritoTemplate")
+const carritoFragmento = document.createDocumentFragment()    // puedo poner = new DocumentFragment() 
+const botonesAgregar = document.querySelectorAll("article button")
+
+const carritoObjeto = {}
+
+
+const agregarFruta = (e) =>{       
+    const producto = {
+        titulo: e.target.dataset.fruta,
+        id: e.target.dataset.fruta,
+        cantidad: 1 
+    }
+    
+    if(carritoObjeto.hasOwnProperty(producto.id)){
+        producto.cantidad = carritoObjeto[producto.id].cantidad + 1
+    }
+    
+    carritoObjeto[producto.id] = producto
+        
+    pintarCarrito()
+}
+
+
+botonesAgregar.forEach((boton) => boton.addEventListener("click",agregarFruta))
+
+
+
+const pintarCarrito = () =>{
+    carrito.textContent=""
+    Object.values(carritoObjeto).forEach(item =>{
+        const cloneTemplate = carritoTemplate.content.firstElementChild.cloneNode(true)
+        cloneTemplate.querySelector(".lead").textContent = item.titulo
+        cloneTemplate.querySelector(".badge").textContent = item.cantidad
+        carritoFragmento.appendChild(cloneTemplate)
+    })
+
+    carrito.appendChild(carritoFragmento)
+}
+
+
+
+
+
+
